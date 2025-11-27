33166c14fdfe37a8c2a7021637004b8093cda6f4
tune <tunedyu@gmail.com>
2025-11-27 19:09:01 +0900

    first commit

---
+++ src/main/java/com/example/demo/LearningPlanApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningPlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningPlanApplication.class, args);
+	}
+
+}
