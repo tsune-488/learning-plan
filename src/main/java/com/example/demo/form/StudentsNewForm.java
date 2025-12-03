586d7750dcd0d04754f034cee1695065441e76f6
tune <tunedyu@gmail.com>
2025-12-03 11:32:11 +0900

    生徒用　フォームとヘルパーの作成

---
+++ src/main/java/com/example/demo/form/StudentsNewForm.java
@@ -0,0 +1,23 @@
+package com.example.demo.form;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+//ToDOフォーム
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentsNewForm {
+	//IDは自動登録のため、入っていません
+	//先生のID　
+	private Integer teacherId;
+	// 出席番号
+    private String studentnumber;
+    //生徒名 
+    private String studentname; 
+    // パスワード
+    private String studentpassword;
+	//新規判定
+    private Boolean isNew;
+}
---
+++ src/main/java/com/example/demo/helper/StudentsHelper.java
@@ -0,0 +1,34 @@
+package com.example.demo.helper;
+
+import com.example.demo.entity.Students;
+import com.example.demo.form.StudentsNewForm;
+
+//新規登録のヘルパー
+public class StudentsHelper {
+	//Studentsへの変換
+	public static Students convertStudents(StudentsNewForm form) {
+		
+		Students students = new Students();
+		students.setTeacherId(form.getTeacherId());
+		students.setStudentnumber(form.getStudentnumber());
+		students.setStudentname(form.getStudentname());
+		students.setStudentpassword(form.getStudentpassword());
+        
+		return students;
+	}
+		
+	//StudentsNewFormへの変換
+	public static StudentsNewForm convertStudentsNewForm(Students students) {
+		
+		StudentsNewForm studentsNewForm = new StudentsNewForm();
+		studentsNewForm.setTeacherId(students.getTeacherId());
+		studentsNewForm.setStudentnumber(students.getStudentnumber());
+		studentsNewForm.setStudentname(students.getStudentname());
+		studentsNewForm.setStudentpassword(students.getStudentpassword());
+		
+		
+		//更新画面設定
+		studentsNewForm.setIsNew(false);
+		return studentsNewForm;
+	}
+}
