043b213a8e5ce2b513aec923e732b9b627f9eccf
tune <tunedyu@gmail.com>
2025-12-04 14:11:08 +0900

    テスト設定画面⇒URL作成⇒生徒ログイン画面　動作OK

---
+++ src/main/java/com/example/demo/controller/StudentTestController.java
@@ -0,0 +1,29 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.example.demo.form.StudentLoginForm;
+
+import lombok.RequiredArgsConstructor;
+
+@Controller
+
+@RequiredArgsConstructor
+public class StudentTestController {
+	
+     // 生徒ページ
+	 @GetMapping("/test/{testId}/student")
+	    public String toStudentLogin(@PathVariable Integer testId, Model model) {
+		    StudentLoginForm form = new StudentLoginForm();
+		    form.setTeacherId(testId);
+		     
+		    model.addAttribute("studentLoginForm", form);
+	        model.addAttribute("testId", testId);
+	        
+	        
+	        return "studentLogin"; 
+	    }
+	}
