202e9d499414e3c24b07e6c91105b66a15e7b1a5
tune <tunedyu@gmail.com>
2025-12-05 20:49:55 +0900

    学習計画と記録　サービスと実装クラス

---
+++ src/main/java/com/example/demo/service/LearningRecordService.java
@@ -0,0 +1,20 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import com.example.demo.entity.LearningRecord;
+
+public interface LearningRecordService {
+	
+	//計画内容および実施内容すべての検索
+	List<LearningRecord> findAll();
+		
+	//IDから登録された情報を取得
+	LearningRecord findById(Integer id);
+		
+	//計画内容および実施内容の登録
+	void insert(LearningRecord learningRecord);
+		
+	//計画内容および実施内容の更新
+	void update(LearningRecord learningRecord);
+}
---
+++ src/main/java/com/example/demo/service/impl/LearningRecordServiceImpl.java
@@ -0,0 +1,42 @@
+package com.example.demo.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.entity.LearningRecord;
+import com.example.demo.repository.LearningRecordMapper;
+import com.example.demo.service.LearningRecordService;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class LearningRecordServiceImpl implements LearningRecordService {
+	
+	//DI
+	private final LearningRecordMapper learningRecordMapper;
+
+	@Override
+	public List<LearningRecord> findAll() {
+		return learningRecordMapper.findAll();
+	}
+
+	@Override
+	public LearningRecord findById(Integer id) {
+		return learningRecordMapper.findById(id);
+	}
+
+	@Override
+	public void insert(LearningRecord learningRecord) {
+		learningRecordMapper.insert(learningRecord);
+	}
+
+	@Override
+	public void update(LearningRecord learningRecord) {
+		learningRecordMapper.update(learningRecord);
+	}
+
+}
