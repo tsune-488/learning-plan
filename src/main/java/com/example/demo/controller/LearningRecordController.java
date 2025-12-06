74e17aac60e0567927776da3af0b2539d2aeb15d
tune <tunedyu@gmail.com>
2025-12-06 19:16:11 +0900

    学習計画　コントローラー

--- src/main/java/com/example/demo/controller/LearningRecordController.java
+++ src/main/java/com/example/demo/controller/LearningRecordController.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import jakarta.servlet.http.HttpServletRequest;
 
@@ -11,7 +13,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.entity.LearningRecord;
+import com.example.demo.entity.TestsSetting;
 import com.example.demo.service.LearningRecordService;
+import com.example.demo.service.TestsService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,6 +25,7 @@ public class LearningRecordController {
 	
 	//DI
 	private final LearningRecordService learningRecordService;
+	private final TestsService testsService;
 	
 	//学習結果と実施内容の登録を実行
 	@PostMapping("/student/save")
@@ -30,12 +35,26 @@ public class LearningRecordController {
 			HttpServletRequest request,
 			Model model) {
 		
+		// テスト期間を取得
+	    TestsSetting testDay = testsService.findById(testId);
+	    LocalDate start = testDay.getStartday();
+	    LocalDate end = testDay.getLastday();
+	        
+	    // 日付期間を作成
+	    List<LocalDate> dateList = new ArrayList<>();
+	    LocalDate today = start;
+	    while (!today.isAfter(end)) {
+	    	dateList.add(today);
+	    	today = today.plusDays(1);
+	    }
+	    
+		
 		//１１日～１６日まで登録
-		for (int day = 11; day <= 16; day++) {
+		for (LocalDate date: dateList) {
 			
 			//HTMLの名前に合わせる
-		    String plan = request.getParameter("plan_" + day);
-		    String record = request.getParameter("record_" + day);
+		    String plan = request.getParameter("plan_" + date);
+		    String record = request.getParameter("record_" + date);
 		    //空なら保存しない
 		    if ((plan == null || plan.isBlank()) && (record == null || record.isBlank())) {
 		        continue;
@@ -45,7 +64,7 @@ public class LearningRecordController {
 		
 		    learningRecord.setStudentId(studentId);
 		    learningRecord.setTestId(testId);
-		    learningRecord.setLearnDay(LocalDate.of(2025, 12, day));
+		    learningRecord.setLearnDay(date);
 		    learningRecord.setPlan(plan);
 		    learningRecord.setRecord(record);
 		
@@ -54,7 +73,7 @@ public class LearningRecordController {
 	    }
 	
 	    model.addAttribute("msg", "保存しました");
-        return "redirect:/students/learning?studentId=" + studentId + "&testId=" + testId; 
+	    return "redirect:/students/learning?studentId=" + studentId + "&testId=" + testId; 
 	}
 	
 	@GetMapping("/students/learning")
@@ -62,9 +81,23 @@ public class LearningRecordController {
 	        @RequestParam Integer studentId,
 	        @RequestParam Integer testId,
 	        Model model) {
+		
+		//テストの日程の取得
+		TestsSetting testDay =  testsService.findById(testId);
+		LocalDate start = testDay.getStartday();
+		LocalDate end = testDay.getLastday();
+		
+		//学習記録期間の日程作成
+		List<LocalDate> dateList = new ArrayList<>();
+		LocalDate toDay = start;
+		while(!toDay.isAfter(end)) {
+			dateList.add(toDay);
+			toDay = toDay.plusDays(1);
+		}
 
 	    model.addAttribute("studentId", studentId);
 	    model.addAttribute("testId", testId);
+	    model.addAttribute("dateList", dateList);
 
 	    return "studentLearning";
 	}
